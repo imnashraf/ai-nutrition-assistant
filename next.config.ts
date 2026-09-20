3bd6d73ceec368f0642a1203c7bf00860a6e6160
Nabil Ashraf <mna.nabil@gmail.com>
2026-09-20 23:44:06 +0400

    fix: explicitly trace onnxruntime-common dependency for vercel

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
       "./node_modules/onnxruntime-node/dist/**/*.js",
       "./node_modules/onnxruntime-node/lib/**/*.js",
       "./node_modules/onnxruntime-node/bin/napi-v6/linux/x64/*",
+      "./node_modules/onnxruntime-common/package.json",
+      "./node_modules/onnxruntime-common/dist/**/*.js",
+      "./node_modules/onnxruntime-common/lib/**/*.js",
     ],
   },
 };
